86ee39106fde41d35e9af63cc063db461723647e
Fedor / 효도르 <fedor@within.co.kr>
2019-08-28 07:14:47 +0900

    elprit project

---
+++ src/main/java/io/github/rusyasoft/example/tour/elprit/elpritreview/ElpritReviewApplication.java
@@ -0,0 +1,13 @@
+package io.github.rusyasoft.example.tour.elprit.elpritreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElpritReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElpritReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/rusyasoft/example/tour/elprit/elpritreview/ElpritReviewApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.rusyasoft.example.tour.elprit.elpritreview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElpritReviewApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
